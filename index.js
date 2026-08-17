ec7cd201917435c85f689cdbb9308a601219ec06
Asif Ullah Sazzad <asifullahsazzad0@gmail.com>
2026-08-18 02:57:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+import express from 'express'
+import cors from 'cors'
+import env from 'dotenv/config'
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const app = express();
+const port = 3000 || process.env.PORT
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+
+
+// Mongodb Database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wlt6tgr.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // Database create
+    const movieDatabase = client.db('MoviePortalDatabase').collection('MoviesCollection')
+
+    // Backend Code
+    app.get('/movies', async (req, res)=> {
+        const cursor = await movieDatabase.find();
+
+        const result = await cursor.toArray();
+
+        res.send(result)
+    })
+
+    // post
+    app.post('/movies', async (req, res) => {
+        const movieData = req.body;
+
+        const result = await movieDatabase.insertOne(movieData);
+
+        res.send(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
